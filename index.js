e2913c37caf55107615d93c966bdc510f72316c8
Soracatd <soracatd@gmail.com>
2025-02-11 12:24:39 +0700

    w6

---
+++ index.js
@@ -0,0 +1,57 @@
+// Get the client
+const mysql = require('mysql2');
+
+// Create the connection to database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307,
+  user: 'root',
+  database: 'week6-1',
+});
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.json());
+app.get('/', (req, res) => {
+    connection.query(
+        'SELECT * FROM `tbl_user` ',
+        function (err, results, fields) {
+          res.json(results);
+    }
+  );
+});
+
+app.get('/:Id', (req, res) => {
+    connection.query(
+        'SELECT * FROM `tbl_user` WHERE Id = ' + req.params.Id,
+        function (err, results, fields) {
+          res.json(results);
+    }
+  );
+});
+
+app.post('/', (req, res) => {
+    const data =req.body;
+    connection.query(
+        'INSERT INTO tbl_user (Fname, Lname, statun)',
+         VALUES ('${data.Fname}', '${data.Lname}', '1'),
+        function (err, results, fields) {
+          res.json(results);
+    }
+  );
+});
+
+app.delete('/:Id', (req, res) => {
+    connection.query(
+        'UPDATE tbl_user SET Statun = 0 WHERE Id = ${req.params.Id}',
+        function (err, results, fields) {
+          res.json(results);
+    }
+  );
+});
+
+app.listen(port, () => {
+    console.log('Server is running on port : ',port);
+})
