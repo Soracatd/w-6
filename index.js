0b4b4de6fef4ee92a50ce5560cf339516e8e2962
Soracatd <soracatd@gmail.com>
2025-02-13 12:26:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-// Get the client
 const mysql = require('mysql2');
+const express = require('express');
 
-// Create the connection to database
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3307,
@@ -9,14 +8,12 @@ const connection = mysql.createConnection({
   database: 'week6-1',
 });
 
-
-const express = require('express');
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.get('/', (req, res) => {
     connection.query(
-        'SELECT * FROM `tbl_user` ',
+        'SELECT * FROM `tbl_user` WHERE statun = 1',
         function (err, results, fields) {
           res.json(results);
     }
@@ -33,14 +30,23 @@ app.get('/:Id', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-    const data =req.body;
+    const data = req.body;
     connection.query(
-        'INSERT INTO tbl_user (Fname, Lname, statun)',
-         VALUES ('${data.Fname}', '${data.Lname}', '1'),
+        'INSERT INTO tbl_user (Fname, Lname, statun) VALUES ('${data.Fname}', '${data.Lname}', '1')',
         function (err, results, fields) {
           res.json(results);
     }
   );
+});
+
+app.put('/:Id', (req, res) => {
+  connection.query(
+      'UPDATE tbl_user SET Fname = ? , Lname = ? WHERE Id = ?',
+      [req.body.Fname, req.body.Lname, req.body.Id],
+      (err, results, fields) => {
+        res.json(results);
+  }
+);
 });
 
 app.delete('/:Id', (req, res) => {
